88c172e3e2c3ff6b272e85c75bedc5a3f58fb632
rafaelcn1 <rafaelcn1@hotmail.com>
2023-05-24 17:26:07 -0300

    feat: criação da classe ClienteDTO

---
+++ helpdesk/src/main/java/com/rafael/helpdesk/dtos/ClienteDTO.java
@@ -0,0 +1,103 @@
+package com.rafael.helpdesk.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rafael.helpdesk.domain.enums.Perfil;
+import com.rafael.helpdesk.domain.model.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	/*
+	 * Classe criada para boas praticas, para que a Entidade (classe) esteja
+	 * expostas
+	 * 
+	 */
+
+	private static final long serialVersionUID = 1L;
+	protected Integer id;
+	protected String nome;
+	protected String cpf;
+	protected String email;
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+
+	@JsonFormat(pattern = "dd/MM/yyyy") // Formato de data no banco conforme o nosso pattern
+	protected LocalDate dataCriacao = LocalDate.now();
+
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente cliente) {
+		super();
+		this.id = cliente.getId();
+		this.nome = cliente.getNome();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.senha = cliente.getSenha();
+		this.perfis = cliente.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = cliente.getDataCriacao();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfis(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+}
