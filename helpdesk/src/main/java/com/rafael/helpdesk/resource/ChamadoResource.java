75a9f8011f5b11587641109b91733ccd95eef53c
rafaelcn1 <rafaelcn1@hotmail.com>
2023-05-26 14:03:01 -0300

    feat: criação do metodo listar todos chamados

--- helpdesk/src/main/java/com/rafael/helpdesk/resource/ChamadoResource.java
+++ helpdesk/src/main/java/com/rafael/helpdesk/resource/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.rafael.helpdesk.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,14 @@ public class ChamadoResource {
 		Chamado chamado = chamadoService.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(chamado));
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> findAll = chamadoService.findAll();
+		List<ChamadoDTO> findAllChamadoDTO = findAll.stream().map(chamado -> new ChamadoDTO(chamado))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(findAllChamadoDTO);
 	}
 
 }
--- helpdesk/src/main/java/com/rafael/helpdesk/services/ChamadoService.java
+++ helpdesk/src/main/java/com/rafael/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.rafael.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,13 @@ public class ChamadoService {
 
 	public Chamado findById(Integer id) {
 		Optional<Chamado> findById = chamadoRepository.findById(id);
-		
+
 		return findById.orElseThrow(() -> new ObjectNotFoundException("Objeto do ID: " + id + " - Não encontrado!"));
-		
+
+	}
+
+	public List<Chamado> findAll() {
+		return chamadoRepository.findAll();
 	}
 
 }
