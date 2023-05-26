03df95ff5bdb7876075e2ff7394672354a19fa40
rafaelcn1 <rafaelcn1@hotmail.com>
2023-05-26 13:49:21 -0300

    feat: criação da classe resource e service do cliente

---
+++ helpdesk/src/main/java/com/rafael/helpdesk/resource/ClienteResource.java
@@ -0,0 +1,28 @@
+package com.rafael.helpdesk.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.helpdesk.domain.model.Cliente;
+import com.rafael.helpdesk.dtos.ClienteDTO;
+import com.rafael.helpdesk.services.ClienteService;
+
+@RestController // permite que métodos retornem diretamente objetos convertidos em JSON/XML
+@RequestMapping(value = "/clientes") // Mapeamento de uma URL para um método específico em uma classe de controle
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> findById(@PathVariable Integer id) {
+		Cliente cliente = clienteService.findById(id);
+		return ResponseEntity.ok().body(new ClienteDTO(cliente));
+
+	}
+
+}
---
+++ helpdesk/src/main/java/com/rafael/helpdesk/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.rafael.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.helpdesk.domain.model.Cliente;
+import com.rafael.helpdesk.repositories.ClienteRepository;
+import com.rafael.helpdesk.services.execptions.ObjectNotFoundException;
+
+@Service // permite a implementação de lógica de negócio e regras de processamento de
+			// dados
+public class ClienteService {
+
+	@Autowired
+	ClienteRepository clienteRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto do ID: " + id + " - Não encontrado!"));
+
+	}
+
+}
